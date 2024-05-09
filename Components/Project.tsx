637ff4d41c464586e4702d6aaa95e179089cdd84
NannKhaingShweYeeLin <124340583+NannKhaingShweYeeLin@users.noreply.github.com>
2024-05-09 21:53:09 +0630

    Update Project.tsx

--- Components/Project.tsx
+++ Components/Project.tsx
@@ -404,53 +404,7 @@ const Project = () => {
 
 
  {/* 2nd */}
- <div className="grid grid-cols-1 md:grid-cols-2 w-[80%] mx-auto gap-[3rem] items-center">
-                <div>
-
-                    {/* <h2 className="text-[15px] md:text-[20px] lg:text-[45px] md:leading-[3rem] leading-[2rem] capitalize mb-[3rem] font-bold text-white">
-          MERN PROJECT <span className="text-yellow-400"></span>
-        </h2> */}
-                    <h1 className="text-[20px] font-bold uppercase text-[#55e6a5] md-[1rem]">
-                        In a box
-                    </h1>
-                    <div className="mb-[3rem] flex items-center md:space-x-10">
-                        <span className="w-[100px] hidden md:block h-[5px] bg-slate-400 rounded-sm"></span>
-                        <p className="text-[19px] text-slate-300 w-[80%]">
-                            {" "}
-                            In a Box, a food delivery company, aims to optimize its Information Technology infrastructure.
-                            Consequently, the company has embarked on the development of a website to enhance its online presence.
-                             As a member of a contracted web development team, I specialize in frontend development, employing HTML 
-                             and CSS to design and create the website
-                        </p>
-                    </div>
-
-
-
-                    <div className='mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6'>
-                        <button className='px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2'>
-                            <Link href="https://github.com/NannKhaingShweYeeLin/In-a-box"><p>Github Link </p></Link>
-                        </button>
-                        <button className='flex items-center space-x-2 px-[2rem] hover:bg-yellow-400 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black'>
-                            <Link href=""><p>Demo </p></Link>
-                        </button>
-                    </div>
-
-                </div>
-                <div
-                    data-aos="fade-left"
-                    className="lg:w-[500px] mx-auto md:mx-0 mt-[2rem] lg:mt-0 lg:h-[500px] w-[300px] h-[300px] relative"
-                >
-                    <Image
-                        src="/images/cc.PNG"
-                        alt="user"
-                        fill
-                        className="relative z-[11] w-[100%] h-[100%]
-                  object-contain"
-                    />
-                    {/* <div className="absolute w-[100%] h-[100%] z-[10] bg-[#55e6a5] top-[-2rem] right-[-2rem]"></div> */}
-                </div>
-            </div>
-            &nbsp; &nbsp; &nbsp;
+ 
 
             {/* 3rd */}
             <div className="grid grid-cols-1 md:grid-cols-2 w-[80%] mx-auto gap-[3rem] items-center">
